09bdf3bd54592015407ef0ffa0a454c20719c588
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:26:00 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan9/teks/PBOIF210118079Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
